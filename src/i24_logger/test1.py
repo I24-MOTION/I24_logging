3fda39aa0ecbbefdfa3711bca56ecbee280f6250
Derek <derekgloudemans@gmail.com>
2022-05-10 13:04:23 -0500

    Minor tweak

--- src/i24_logger/test1.py
+++
@@ -1,8 +0,0 @@
-
-import log_writer
-log_writer.connect_automatically(user_settings = {"log_name":"new name"})
-logger = log_writer.logger
-# for x in range(1000):
-#     logger.critical("Hello Zi")
-
-logger.debug("Test")
